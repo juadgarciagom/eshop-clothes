7d14fdeeaa9f30e524b81a09d353f109e219e863
juadgarciagom <juadgarciagom@outlook.com>
2022-01-19 18:39:40 -0500

    Fixed fields on the homepage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,36 +5,36 @@ import './homepage.scss';
 export const HomePage = () => {
   return (
     <div className="homepage">
-      {/* <h1>Bienvenido al e-shop</h1> */}
+      <h1>Bienvenido al e-shop</h1>
       <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Sombreros</h1>
           </div>
         </div>
-      </div>
-      <div className="directory-menu">
+      
+      
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Tenis</h1>
           </div>
         </div>
-      </div>
-      <div className="directory-menu">
+     
+      
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Chaquetas</h1>
           </div>
         </div>
-      </div>
-      <div className="directory-menu">
+      
+      
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Hombres</h1>
           </div>
         </div>
-      </div>
-      <div className="directory-menu">
+      
+      
         <div className="menu-item">
           <div className="content">
             <h1 className="title">Mujeres</h1>
