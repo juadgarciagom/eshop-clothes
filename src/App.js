d0d000afbc74f28f6b8b479c375ed4ec902d3ca3
juadgarciagom <juadgarciagom@outlook.com>
2022-01-20 10:16:48 -0500

    Made components for the homepage and for the fields in that

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { HomePage } from "./HomePage";
+import { HomePage } from "./pages/homepage/Homepage";
+
 
 function App() {
   return (
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/directory/Directory";
+
+import './homepage.scss';
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <h1>Bienvenido al e-shop</h1>
+      <Directory />
+    </div>
+  );
+};
