ff14c0dd681fb682ecd4c6d8aceb09de11da111c
juadgarciagom <juadgarciagom@outlook.com>
2022-01-23 17:37:59 -0500

    Fixed some vulnerabilities with postcss and nth-check

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { MenuItem } from '../menu-item/MenuItem';
-import './directory.scss';
+import React, { Component } from "react";
+import { MenuItem } from "../menu-item/MenuItem";
+import "./directory.scss";
 
 class Directory extends Component {
     constructor() {
@@ -9,50 +9,51 @@ class Directory extends Component {
         this.state = {
             sections: [
                 {
-                  title: 'Sombreros',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1
+                    title: "Sombreros",
+                    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+                    id: 1,
                 },
                 {
-                  title: 'Chaquetas',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2
+                    title: "Chaquetas",
+                    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+                    id: 2,
                 },
                 {
-                  title: 'Tenis',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3
+                    title: "Tenis",
+                    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+                    id: 3,
                 },
                 {
-                  title: 'Mujeres',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4
+                    title: "Mujeres",
+                    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+                    size: "large",
+                    id: 4,
                 },
                 {
-                  title: 'Hombres',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5
-                }
-              ]
-        }
+                    title: "Hombres",
+                    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+                    size: "large",
+                    id: 5,
+                },
+            ],
+        };
     }
 
     render() {
         //Functionality of the destructuracion of the sections with the map method
-        this.state.sections.map(({imageUrl}) => (
-            console.log(imageUrl)
-        ));
+        this.state.sections.map(({ imageUrl }) => console.log(imageUrl));
         return (
-        <div className="directory-menu">
-            {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key = {id} title={title} imageUrl={imageUrl} size={size} />
-                ))
-            }
-        </div>
-        )
+            <div className="directory-menu">
+                {this.state.sections.map(({ title, imageUrl, id, size }) => (
+                    <MenuItem
+                        key={id}
+                        title={title}
+                        imageUrl={imageUrl}
+                        size={size}
+                    />
+                ))}
+            </div>
+        );
     }
 }
 
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Directory from "../../components/directory/Directory";
 
-import './homepage.scss';
+import "./homepage.scss";
 
 export const HomePage = () => {
-  return (
-    <div className="homepage">
-      <h1>Bienvenido al e-shop</h1>
-      <Directory />
-    </div>
-  );
+    return (
+        <div className="homepage">
+            <h1>Bienvenido al e-shop</h1>
+            <Directory />
+        </div>
+    );
 };
