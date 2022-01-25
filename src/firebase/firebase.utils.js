24512765a71b723331a70a004ae1b1870e0e014b
juadgarciagom <juadgarciagom@outlook.com>
2022-01-25 18:05:24 -0500

    Integrated firestore to our sign in page so we could store users in it

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,35 @@ const config = {
     measurementId: "G-3YSB0PPRDH",
 };
 
+// Function to get the user from firebase, CRUD methods only work in document reference and with the exists property we know if 
+// snapshot exists and if that we created an user with the user authentication created
+export const createUserProfileDocument = async (userAuth, additonalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additonalData
+            })
+        } catch (error) {
+            console.log('Hay un error creando el usuario' , error.message);
+        }
+    }
+
+    return userRef;
+
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
