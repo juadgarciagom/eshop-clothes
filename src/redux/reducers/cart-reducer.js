94ebe45049318615e522211704d98f689ce157ce
juadgarciagom <juadgarciagom@outlook.com>
2022-01-31 16:53:20 -0500

    Added add quantity or less quantity of items in our checkout page

--- src/redux/reducers/cart-reducer.js
+++ src/redux/reducers/cart-reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from "../constants/CartTypes";
-import { addItemToCart } from "../utils/cart-utils";
+import { addItemToCart, removeItemFromCart } from "../utils/cart-utils";
 
 //We want that the initial state is that the cart dropdown be hidden
 const INITIAL_STATE = {
@@ -18,6 +18,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+            case CartActionTypes.REMOVE_ITEM: 
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id) //If our cart item is different from the payload that the action is made keep the array (comparing the id)
             }
             
     
